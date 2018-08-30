3b50d037e29096bbf0bf1865a033071eb86e9f54
zhangpengpeng1 <zhangpengpeng1@xiaomi.com>
2018-08-30 15:37:52 +0800

    remove source file

--- src/data_pre.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-path = '/home/zhangpengpeng/.keras/datasets/imdb.npz'
-with np.load(path) as f:
-    x_train, labels_train = f['x_train'], f['y_train']
-    x_test, labels_test = f['x_test'], f['y_test']
-    print(len(x_train[9]))
-    print(labels_train[0])
